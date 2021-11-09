49a863543d7b1343354b07f634ade82812a6e55d
alibekanu17 <93991522+alibekanu17@users.noreply.github.com>
2021-11-09 19:56:01 +0600

    site
    about com[uter

---
+++ main.js
@@ -0,0 +1,14 @@
+$('.header__burger').on('click', function(e) {
+    e.preventDefault();
+    $('.header-links').toggleClass('header-links__active');
+    
+});
+$('.close').on('click', function(e) {
+    e.preventDefault();
+    $('.header-links').toggleClass('header-links__active');
+    
+});
+
+
+
+
